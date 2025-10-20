9ff69607cbe0c219ef97a01f4404da47a614802d
Derick S. Park <145691757+derickspark@users.noreply.github.com>
2025-10-20 20:13:51 +0900

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,12 @@
 # R/zzz.R
 
 .onAttach <- function(libname, pkgname) {
-  # 패키지 attach 시 데이터셋 자동 로드
-  utils::data("cty", package = "vworldgeocodeR", envir = parent.env(environment()))
-  utils::data("admi", package = "vworldgeocodeR", envir = parent.env(environment()))
-  utils::data("mega", package = "vworldgeocodeR", envir = parent.env(environment()))
-
-  packageStartupMessage(
-    "cty, admi, mega 데이터셋이 자동으로 로드되었습니다. (vworldgeocodeR)"
-  )
+  # 패키지 attach 시 전역환경(.GlobalEnv)으로 자동 로드
+  for (nm in c("cty", "admi", "mega")) {
+    try(
+      utils::data(list = nm, package = pkgname, envir = .GlobalEnv),
+      silent = TRUE
+    )
+  }
+  packageStartupMessage("cty, admi, mega 데이터셋을 .GlobalEnv로 자동 로드했습니다. (vworldgeocodeR)")
 }
